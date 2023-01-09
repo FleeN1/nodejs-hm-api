3920d7913d4137449617df04043f443d3d5f16d6
Makievskiy Dmytro <dimamakievskii@gmail.com>
2023-01-09 14:54:37 +0200

    hw-email

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const createError = require('./createError');
 const createHashPass = require('./createHashPass');
+const sendMail = require('./sendMail');
 
 module.exports = {
   createError,
-  createHashPass
+  createHashPass,
+  sendMail,
 };
